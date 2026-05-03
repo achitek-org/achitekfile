f10f9ed4bc5e28a580e27436b7cbb38e7fe89883
Samuel Joli <samuel.joli.ftn@gmail.com>
2026-05-02 21:43:29 -0400

    feat: define source range and diagnostic primitives (#12)
    closes #4

--- src/lib.rs
+++ src/lib.rs
@@ -32,10 +32,14 @@
 #![deny(missing_docs)]
 
 mod ast;
+mod diagnostics;
 mod parser;
 mod sort;
 
 pub use ast::{
     AchitekAst, AstError, ComparisonOperator, Dependency, Prompt, PromptType, Validation, Value,
+};
+pub use diagnostics::{
+    Diagnostic, DiagnosticCode, DiagnosticKind, Severity, TextPosition, TextRange,
 };
 pub use parser::{ParseError, from_str};
